ddd55416ba922200823494f1fd4273d3865908fd
Vicky Ungemach <vicky.ungemach@gmail.com>
2020-09-20 12:35:47 -0700

    updated projects

--- client/src/components/sections/projects/Dashboard.js
+++ client/src/components/sections/projects/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
             <h3>Hill Brewing Co Dashboard</h3>
             <p>Created a REST API to manage the homepage's data and a dashboard where members can create, edit and delete
           recipes displayed on the homepage.</p>
-            <p><span id="createdwith">Created with </span><span id="tools">&nbsp;Node.js, Express, MongoDB, React.js</span></p>
+            <p><span id="createdwith">Created with </span><span id="tools">&nbsp;Node.js, Express, MongoDB, React.js, Redux</span></p>
             <a target="_blank" href="https://www.hillbrewingco.net/register"><button class="hillbrewing-btn">LIVE VERSION</button></a>
             <a target="_blank" href="https://github.com/vickyungemach/hillbrewingco"><button class="hillbrewing-btn" id="github"> GITHUB</button></a>
         </div>
